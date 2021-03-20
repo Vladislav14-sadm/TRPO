b51c1b5618efe48b768f330b57b60a19d1295729
Vladislav14-sadm <vladakulov14@mail.ru>
2021-03-20 18:35:34 +0300

    labar10

--- Akulov/AkulovException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Akulov;
-
-class AkulovException extends \RuntimeException
-{
-
-}
--- index.php
+++ index.php
@@ -2,20 +2,26 @@
 ini_set("display_errors", 1);
 error_reporting(-1);
 
-use Akulov\Square as B;
-use Akulov\MyLog as M;
-use Akulov\AkulovException;
+/*use akulov\Square as B;
+use akulov\MyLog as M;
+use akulov\AkulovException;*/
 
-require_once 'core/EquationInterface.php';
+require_once __DIR__ . './vendor/autoload.php';
+
+/*require_once 'core/EquationInterface.php';
 require_once 'core/LogAbstract.php';
 require_once 'core/LogInterface.php';
 require_once 'Akulov/Line.php';
 require_once 'Akulov/Square.php';
 require_once 'Akulov/MyLog.php';
-require_once 'Akulov/AkulovException.php';
+require_once 'Akulov/AkulovException.php';*/
 
 try {
-    M::log("Версия программы: " . trim(file_get_contents('version')) );
+    if(!is_dir("log")) {
+        mkdir("log", 0700);
+    }
+	
+	akulov\MyLog::log("Версия программы: " . trim(file_get_contents('version')) );
 
     echo "Введите 3 числа через Enter\n\r";
 
@@ -23,13 +29,12 @@ try {
     $b = (int)readline();
     $c = (int)readline();
 
-    M::log("The equation is introduced:" . " $a*x^2 + $b*x + $c = 0");
-    $test = new B();
+    akulov\MyLog::log("The equation is introduced:" . " $a*x^2 + $b*x + $c = 0");
+    $test = new akulov\Square();
     $r = $test->solve($a, $b, $c);
-    M::log("Roots: " . implode(", ", $r));
+    akulov\MyLog::log("Roots: " . implode(", ", $r));
 
-} catch (AkulovException $e) {
-    M::log($e->getMessage());
+} catch ( akulov\AkulovException $e) {
+    akulov\MyLog::log($e->getMessage());
 }
-M::write();
-
+akulov\MyLog::write();
